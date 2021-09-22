7cb9443ee2f8d09647ebd7b7f15aaace860f12ca
Joan Bena <78666333+JoanBena@users.noreply.github.com>
2021-09-22 22:38:23 +0200

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+//DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "joanbena2020");
+define("DB_NAME", "internship");
+
+
+// define("SITE_TITLE", "JobLister");
+//Paths
